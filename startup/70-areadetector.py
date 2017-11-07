9b9c76e648800026e2414304b8c223e76f9bd0ed
Pete Jemian <prjemian@gmail.com>
2017-11-07 17:37:14 -0600

    w-i-p

--- startup/70-areadetector.py
+++ startup/70-areadetector.py
@@ -2,33 +2,33 @@ print(__file__)
 
 from ophyd import SingleTrigger, AreaDetector, SimDetector
 from ophyd import HDF5Plugin
-from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
+#from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
 from ophyd import Component, Device, EpicsSignalWithRBV
 from ophyd.areadetector import ADComponent
 
 
-class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
-	
-	file_number_sync = None
-	
-	def get_frames_per_point(self):
-		return self.parent.cam.num_images.get()
-	
+#class MyHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
+#	
+#	file_number_sync = None
+#	
+#	def get_frames_per_point(self):
+#		return self.parent.cam.num_images.get()
 
-class MyDetector(SingleTrigger, SimDetector):
-	
-	hdf1 = Component(
-		MyHDF5Plugin, 
-		"HDF1:", 
-		root="/", 					# for databroker filestore
-		write_path_template="/tmp",	# for EPICS area detector
-		)
+
+#class MyDetector(SingleTrigger, SimDetector):
+#	
+#	hdf1 = Component(
+#		MyHDF5Plugin, 
+#		"HDF1:", 
+#		root="/", 					# for databroker filestore
+#		write_path_template="/tmp",	# for EPICS area detector
+#		)
 
 try:
-	simdet = MyDetector('13SIM1:', name='simdet')
-	simdet.read_attrs = ['hdf1', 'cam']
-	simdet.hdf1.read_attrs = []  # 'image' gets added dynamically
-	#simdet = SimDetector('13SIM1:')
+	#simdet = MyDetector('13SIM1:', name='simdet')
+	#simdet.read_attrs = ['hdf1', 'cam']
+	#simdet.hdf1.read_attrs = []  # 'image' gets added dynamically
+	simdet = SimDetector('13SIM1:')
 except TimeoutError:
 	print("Could not connect 13SIM1: sim detector")
 
