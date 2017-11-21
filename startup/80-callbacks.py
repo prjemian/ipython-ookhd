882603c72fa5064e3803420f8c430bb69582da9d
Pete Jemian <prjemian@gmail.com>
2017-11-21 09:15:16 -0600

    add SPEC file writer callback

--- startup/80-callbacks.py
+++ startup/80-callbacks.py
@@ -3,6 +3,12 @@ print(__file__)
 # custom callbacks
 
 import APS_BlueSky_tools.callbacks
+import APS_BlueSky_tools.filewriters
 
 doc_collector = APS_BlueSky_tools.callbacks.DocumentCollectorCallback()
 callback_db['doc_collector'] = RE.subscribe(doc_collector.receiver)
+
+specwriter = APS_BlueSky_tools.filewriters.SpecWriterCallback()
+# for developer, write the SPEC file to /tmp (assumes OS is Linux)
+specwriter.newfile(os.path.join("/tmp", specwriter.spec_filename))
+callback_db['specwriter'] = RE.subscribe(specwriter.receiver)
