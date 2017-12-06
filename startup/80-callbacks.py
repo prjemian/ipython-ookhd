8bca58c737dc0d310600b59bfdcba989c69905a1
Pete Jemian <prjemian@gmail.com>
2017-12-05 20:32:20 -0600

    show how to run the 0MQ example

--- startup/80-callbacks.py
+++ startup/80-callbacks.py
@@ -4,6 +4,8 @@ print(__file__)
 
 import APS_BlueSky_tools.callbacks
 import APS_BlueSky_tools.filewriters
+from APS_BlueSky_tools.zmq_pair import ZMQ_Pair, mona_zmq_sender
+
 
 doc_collector = APS_BlueSky_tools.callbacks.DocumentCollectorCallback()
 callback_db['doc_collector'] = RE.subscribe(doc_collector.receiver)
@@ -13,3 +15,39 @@ callback_db['specwriter'] = RE.subscribe(specwriter.receiver)
 # for developer, write the SPEC file to /tmp (assumes OS is Linux)
 specwriter.newfile(os.path.join("/tmp", specwriter.spec_filename))
 print("SPEC data file:", specwriter.spec_filename)
+
+
+
+class MonaCallback0MQ(object):
+    """
+    My BlueSky 0MQ talker to send *all* documents emitted
+    """
+    
+    def __init__(self, host=None, port=None, detector=None):
+        self.talker = ZMQ_Pair(host or "localhost", port or "5556")
+        self.detector = detector
+    
+    def end(self):
+        """ZMQ client tells the server to end the connection"""
+        self.talker.end()
+
+    def receiver(self, key, document):
+        """receive from RunEngine, send from 0MQ talker"""
+        mona_zmq_sender(self.talker, key, document, self.detector)
+
+
+def demo_start_mona_callback_as_zmq_client():
+    """
+    show how to use this code with the MONA project
+
+    First: be sure the ZMQ server code is already running (outside of BlueSky).
+    Then, run this code.  If the server is not running, this code may fail.
+    """
+    for key in "doc_collector specwriter zmq_talker".split():
+        if key in callback_db:
+            RE.unsubscribe(callback_db[key])
+            del callback_db[key]
+    zmq_talker = MonaCallback0MQ(detector=plainsimdet.image)
+    callback_db['zmq_talker'] = RE.subscribe(zmq_talker.receiver)
+    RE(bp.count([plainsimdet], num=2))
+    return zmq_talker
