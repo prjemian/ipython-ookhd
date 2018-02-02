44799c20328ff5913b6463d392654fcec6fd902e
Pete Jemian <prjemian@gmail.com>
2018-02-02 12:18:41 -0600

    DemoShutter now working

--- profile_bluesky/startup/21-demoshutter.py
+++ profile_bluesky/startup/21-demoshutter.py
@@ -4,10 +4,17 @@ print(__file__)
 
 import threading
 
-class DemoShutter(Device):
-    open_bit = Component(EpicsSignal, bit1.pvname)
-    close_bit = Component(EpicsSignal, bit2.pvname)
-    delay_s = 1.0
+class ApsPssShutter(Device):
+    """
+    APS PSS shutter
+    
+    * shutters have separate bit PVs for open and close
+    * set the bit, the shutter moves, and the bit resets a short time later
+    * no indication that the shutter has actually moved from the bits
+    """
+    open_bit = Component(EpicsSignal, ":open")
+    close_bit = Component(EpicsSignal, ":close")
+    delay_s = 1.2
     valid_open_values = ["open",]   # lower-case strings ONLY
     valid_close_values = ["close",]
     
@@ -22,34 +29,37 @@ class DemoShutter(Device):
     def set(self, value, **kwargs):
         """open or close the shutter, BlueSky plan use"""
         # ensure numerical additions to lists are now strings
-        self.valid_open_values = list(map(str.lower, map(str, self.valid_open_values)))
-        self.valid_close_values = list(map(str.lower, map(str, self.valid_close_values)))
+        self.valid_open_values = [str(i).lower() for i in self.valid_open_values]
+        self.valid_close_values = [str(i).lower() for i in self.valid_close_values]
+
+        acceptables = self.valid_open_values + self.valid_close_values
+        if str(value).lower() not in acceptables:
+            msg = "value should be one of " + " | ".join(acceptables)
+            msg += " : received " + str(value)
+            raise ValueError(msg)
         
         status = DeviceStatus(self)
-        print("start", status)
         
         def move_shutter():
             if str(value).lower() in self.valid_open_values:
-                yield from mv(self.open_bit, 1)
+                self.open()     # no need to yield inside a thread
             elif str(value).lower() in self.valid_close_values:
-                yield from mv(self.close_bit, 1)
-            else:
-                acceptables = self.valid_open_values + self.valid_close_values
-                msg = "value should be one of " + str(acceptables)
-                msg += " : received " + str(value)
-                raise ValueError(msg)
+                self.close()
         
         def run_and_delay():
-            print("run_and_delay", thread, status)
             move_shutter()
-            #yield from sleep(self.delay_s)
+            # sleep, since we don't *know* when the shutter has moved
+            sleep(self.delay_s)
             status._finished(success=True)
-            print("thread complete", thread, status)
         
-        thread = threading.Thread(target=run_and_delay, daemon=True)
-        thread.start()
-        print("thread started", thread, status)
+        threading.Thread(target=run_and_delay, daemon=True).start()
         return status
+
+
+class DemoShutter(ApsPssShutter):
+    """variation of ApsPssShutter for testing"""
+    open_bit = Component(EpicsSignal, bit1.pvname)
+    close_bit = Component(EpicsSignal, bit2.pvname)
 
 
 shtr = DemoShutter(name="shtr")
