e278f28818c2b6837efec1f1df3bf3e3367a0932
Pete Jemian <prjemian@gmail.com>
2018-02-02 12:48:24 -0600

    finished demo

--- profile_bluesky/startup/21-demoshutter.py
+++ profile_bluesky/startup/21-demoshutter.py
@@ -1,6 +1,8 @@
 print(__file__)
 
 """test a demo shutter"""
+from APS_BlueSky_tools.devices import ApsPssShutter
+
 
 import threading
 
@@ -8,32 +10,68 @@ class ApsPssShutter(Device):
     """
     APS PSS shutter
     
-    * shutters have separate bit PVs for open and close
-    * set the bit, the shutter moves, and the bit resets a short time later
+    * APS PSS shutters have separate bit PVs for open and close
+    * set either bit, the shutter moves, and the bit resets a short time later
     * no indication that the shutter has actually moved from the bits
+    
+    USAGE:
+    
+        shutter_a = ApsPssShutter("2bma:A_shutter", name="shutter")
+        
+        shutter_a.open()
+        shutter_a.close()
+        
+        shutter_a.set("open")
+        shutter_a.set("close")
+        
+    When using the shutter in a plan, be sure to use `yield from`.
+
+        def in_a_plan(shutter):
+            yield from abs_set(shutter, "open", wait=True)
+            # do something
+            yield from abs_set(shutter, "close", wait=True)
+        
+        RE(in_a_plan(shutter_a))
+        
+    The strings accepted by `set()` are defined in two lists:
+    `valid_open_values` and `valid_close_values`.  These lists
+    are treated (internally to `set()`) as lower case strings.
+    
+    Example, add "o" & "x" as aliases for "open" & "close":
+    
+        shutter_a.valid_open_values.append("o")
+        shutter_a.valid_close_values.append("x")
+        shutter_a.set("o")
+        shutter_a.set("x")
     """
     open_bit = Component(EpicsSignal, ":open")
     close_bit = Component(EpicsSignal, ":close")
     delay_s = 1.2
     valid_open_values = ["open",]   # lower-case strings ONLY
     valid_close_values = ["close",]
+    busy = Signal(value=False, name="busy")
     
     def open(self):
-        """open the shutter, interactive use"""
+        """request shutter to open, interactive use"""
         self.open_bit.put(1)
     
     def close(self):
-        """close the shutter, interactive use"""
+        """request shutter to close, interactive use"""
         self.close_bit.put(1)
     
     def set(self, value, **kwargs):
-        """open or close the shutter, BlueSky plan use"""
+        """request the shutter to open or close, BlueSky plan use"""
         # ensure numerical additions to lists are now strings
-        self.valid_open_values = [str(i).lower() for i in self.valid_open_values]
-        self.valid_close_values = [str(i).lower() for i in self.valid_close_values]
+        def input_filter(v):
+            return str(v).lower()
+        self.valid_open_values = list(map(input_filter, self.valid_open_values))
+        self.valid_close_values = list(map(input_filter, self.valid_close_values))
+        
+        if self.busy.value:
+            raise RuntimeError("shutter is operating")
 
         acceptables = self.valid_open_values + self.valid_close_values
-        if str(value).lower() not in acceptables:
+        if input_filter(value) not in acceptables:
             msg = "value should be one of " + " | ".join(acceptables)
             msg += " : received " + str(value)
             raise ValueError(msg)
@@ -41,15 +79,17 @@ class ApsPssShutter(Device):
         status = DeviceStatus(self)
         
         def move_shutter():
-            if str(value).lower() in self.valid_open_values:
+            if input_filter(value) in self.valid_open_values:
                 self.open()     # no need to yield inside a thread
-            elif str(value).lower() in self.valid_close_values:
+            elif input_filter(value) in self.valid_close_values:
                 self.close()
         
         def run_and_delay():
+            self.busy.put(True)
             move_shutter()
             # sleep, since we don't *know* when the shutter has moved
-            sleep(self.delay_s)
+            time.sleep(self.delay_s)
+            self.busy.put(False)
             status._finished(success=True)
         
         threading.Thread(target=run_and_delay, daemon=True).start()
