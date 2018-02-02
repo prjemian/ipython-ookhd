832d3dc26b978e31b6b59c3f6c870127304f515c
Pete Jemian <prjemian@gmail.com>
2018-02-02 11:12:11 -0600

    simulate a PSS shutter PV

---
+++ profile_bluesky/startup/20-testing.py
@@ -0,0 +1,71 @@
+print(__file__)
+
+"""test some ideas"""
+
+class SseqRecord(Device):
+    dol1 = Component(EpicsSignal, ".DOL1")
+    dly1 = Component(EpicsSignal, ".DLY1")
+    lnk1 = Component(EpicsSignal, ".LNK1")
+    flnk = Component(EpicsSignal, ".FLNK")
+    enable = Component(EpicsSignal, "Enable")
+    enable_calc = Component(EpicsSignal, "EnableCalc")
+
+"""
+demo of how PSS shutter works
+
+set open bit, PSS resets it a short time later
+
+xxx:bit1 will act like OPEN bit
+xxx:bit2 will act like CLOSE bit
+There is no status bit
+"""
+RESET_DELAY_S = 1.0
+
+bit1 = EpicsSignal("xxx:bit1", name="bit1")
+bit2 = EpicsSignal("xxx:bit2", name="bit2")
+
+calc1 = calcs.calc1
+calc2 = calcs.calc2
+
+sseq_enable = EpicsSignal("xxx:userStringSeqEnable", name="sseq_enable")
+sseq1 = SseqRecord("xxx:userStringSeq1", name="sseq1")
+sseq2 = SseqRecord("xxx:userStringSeq2", name="sseq2")
+
+sseq_enable.put(1)
+
+def initBitReset(bit, sseq, calc):
+    """
+    setting bit to 1 waits a short delay, then resets the bit
+    """
+    # FIXME: Why is this needed?  synApps question
+    __calc_enable = EpicsSignal(calc.prefix+"EnableCalc", name="__calc_enable")
+    __calc_enable.put(1)
+    
+    sseq.enable.put(1)
+    sseq.enable_calc.put(1)
+    
+    calc.outn.put(bit.pvname)
+    sseq.dol1.put(bit.pvname + " CP NMS")
+    sseq.dly1.put(RESET_DELAY_S)
+    sseq.lnk1.put(calc.channels.A.value.pvname + " CP NMS")
+    sseq.flnk.put(calc.prefix)
+
+initBitReset(bit1, sseq1, calc1)
+initBitReset(bit2, sseq2, calc2)
+
+
+def planB():
+    # yield from mv(bit1, 1, bit2, 1)
+    # yield from mv(bit1, 1)
+    # yield from mv(bit2, 1)
+    # each bit only set/reset once
+
+    # TODO: need to re-implement with wait
+    yield from abs_set(bit1, 1, group="A")
+    yield from abs_set(bit2, 1, wait=True)
+    print("group A")
+    yield from abs_set(bit1, 1, wait=True)
+    print("group B")
+    yield from abs_set(bit2, 1, wait=True)
+    print("group C")
+
