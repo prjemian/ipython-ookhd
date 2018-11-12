e0c4e4fe23221b75b9b007c572864f57f9375ce2
Pete Jemian <prjemian@gmail.com>
2018-11-12 10:33:04 -0600

    switch to ScalerCH device

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -2,10 +2,19 @@ print(__file__)
 
 """various detectors and other signals"""
 
+from ophyd.scaler import ScalerCH
 from APS_BlueSky_tools.examples import SynPseudoVoigt
+import APS_BlueSky_tools.devices as APS_devices
 
 noisy = EpicsSignalRO('xxx:userCalc1', name='noisy')
-scaler = EpicsScaler('xxx:scaler1', name='scaler')
+# scaler = EpicsScaler('xxx:scaler1', name='scaler')
+scaler = ScalerCH("xxx:scaler1", name="scaler")
+scaler.channels.chan04.chname.put("scint")
+scaler.channels.chan07.chname.put("roi1")
+
+scaler.match_names()
+APS_devices.use_EPICS_scaler_channels(scaler)
+
 
 synthetic_pseudovoigt = SynPseudoVoigt(
     'synthetic_pseudovoigt', m1, 'm1', 
