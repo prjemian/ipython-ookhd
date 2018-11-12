a6171060896430c1966affe25f9f99196650529a
Pete Jemian <prjemian@gmail.com>
2018-11-12 10:12:13 -0600

    add DHT22 sensor device

---
+++ profile_bluesky/startup/45-rpi-dht22.py
@@ -0,0 +1,24 @@
+print(__file__)
+
+"""Raspberry Pi with DHT22 sensor running EPICS IOC"""
+
+class DHT22Average(Device):
+    average = Component(EpicsSignal, "")
+    sdev = Component(EpicsSignal, ":sdev")
+
+class RpiDHT22(Device):
+    available = Component(EpicsSignal, "available")
+    status = Component(EpicsSignal, "status")
+    humidity = Component(EpicsSignal, "humidity")
+    temperature = Component(EpicsSignal, "temperature")
+    humidity_1m = Component(DHT22Average, "humidity:1m")
+    humidity_15m = Component(DHT22Average, "humidity:15m")
+    humidity_60m = Component(DHT22Average, "humidity:60m")
+    temperature_1m = Component(DHT22Average, "temperature:1m")
+    temperature_15m = Component(DHT22Average, "temperature:15m")
+    temperature_60m = Component(DHT22Average, "temperature:60m")
+
+try:
+    rpi5bf5 = RpiDHT22("rpi5bf5:0:", name="rpi5bf5")
+except Exception as exc:
+    print("Could not connect RPi sensor:", exc)
